ced21b4df95ce19694d846fe59ee3d2572d4669d
Kelvin Tan <kelvintan@Kelvins-MacBook-Pro.local>
2018-04-18 09:42:26 +0800

    First Commit

--- 7 - Machine Learning/ViewController.swift
+++ 7 - Machine Learning/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            guard let ciimage = CIImage(image:image) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage){
+        //created a model using VNCoreMLModel as the container to get the model
+        //will be used to classify the image
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Mode Failed to process Image")
+            }
+            var percentage = result.first!.confidence * 100
+            self.descriptionLabel.text = "\(percentage)% \(result.first!.identifier)"
 
-
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
 }
 
